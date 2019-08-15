a30394bc39ec1d640591ccf865bc438e241ab166
Vladimir Severov <vsev.s@mail.ru>
2019-08-15 21:32:18 +0300

    [UPD] Deployment fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Coping..."
 scp -i ./deploy_key main.o $MACHINE_USERNAME@$MACHINE_ADDRESS:back/main.o
+echo "Restarting..."
 ssh -i ./deploy_key $MACHINE_USERNAME@$MACHINE_ADDRESS <<EOF
 sudo systemctl restart tarantool-api.service
 EOF
+echo "Finished!"
